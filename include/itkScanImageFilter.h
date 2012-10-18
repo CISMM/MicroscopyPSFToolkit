f78ba62294ceca5568d44a150d3277a4f8f4b154
Cory Quammen <cquammen@cs.unc.edu>
2012-10-18 10:45:55 -0400

    Changed license files to GPL
    We could really release this under a dual license model,
    GPL if the user enable the COSMOS-based PSF models and Apache 2.0
    if the user doesn't. I'm not sure of the best way to do this,
    so we'll just stick with GPL for now. No one is likely to use
    this project without using the PSF models, and the more general
    purpose filters could potentially be added to ITK in the future.

--- include/itkScanImageFilter.h
+++ include/itkScanImageFilter.h
@@ -1,22 +1,21 @@
-/*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkScanImageFilter.h,v $
-  Language:  C++
-  Date:      $Date: 2009/07/26 16:52:06 $
-  Version:   $Revision: 1.1 $
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-  Portions of this code are covered under the VTK copyright.
-  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+/****************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ ****************************************************************************/
 #ifndef __itkScanImageFilter_h
 #define __itkScanImageFilter_h
 
--- include/itkScanImageFilter.hxx
+++ include/itkScanImageFilter.hxx
@@ -1,21 +1,23 @@
-/*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkScanImageFilter.cxx,v $
-  Language:  C++
-  Date:      $Date: 2009/09/08 21:33:37 $
-  Version:   $Revision: 1.2 $
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
-#ifndef __itkScanImageFilter_cxx
-#define __itkScanImageFilter_cxx
+/****************************************************************************
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ ****************************************************************************/
+#ifndef __itkScanImageFilter_hxx
+#define __itkScanImageFilter_hxx
 
 #include <itkImageLinearConstIteratorWithIndex.h>
 #include <itkProgressReporter.h>
